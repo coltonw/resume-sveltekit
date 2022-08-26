b2b9bf123e62e628a3e287c3aad481b1b0f1dcab
Will Colton <coltonw@gmail.com>
2022-08-25 22:47:44 -0400

    :eyes: Links to other implementations

--- src/lib/header/Header.svelte
+++ src/lib/header/Header.svelte
@@ -3,7 +3,7 @@
   import Github from '$lib/svgs/Github.svelte';
 </script>
 
-<header class="mb-12 flex items-center justify-between px-4 py-3 text-base">
+<header class="flex items-center justify-between px-4 py-3 text-base">
   <div class="flex items-center justify-between p-0">
     <div>
       <a href="/">
@@ -17,24 +17,6 @@
     </div>
   </div>
   <nav class="flex p-0">
-    <!-- <a
-      href="https://cra.willcolton.com/"
-      class="block px-2 py-1 text-stone-600 font-semibold rounded hover:text-stone-700"
-    >
-      CRA
-    </a>
-    <a
-      href="https://fresh.willcolton.com"
-      class="mt-1 block px-2 py-1 text-stone-600 font-semibold rounded hover:text-stone-700 sm:mt-0 sm:ml-2"
-    >
-      Deno Fresh
-    </a>
-    <a
-      href="https://svelte.willcolton.com"
-      class="block px-2 py-1 text-stone-600 font-semibold rounded hover:text-stone-700"
-    >
-      Svelte
-    </a> -->
     <a
       href="https://github.com/coltonw"
       class="block px-2 py-1 text-stone-600 rounded hover:text-stone-700"
@@ -43,4 +25,27 @@
       <Github class="h-6 w-6" />
     </a>
   </nav>
+</header>
+<header
+  class="mb-12 flex items-center justify-start px-4 text-base border-b-2 border-stone-200"
+>
+  <div class="hidden sm:block px-2 py-1 text-stone-800">Implementations:</div>
+  <a
+    href="https://cra.willcolton.com/"
+    class="block px-2 py-1 text-stone-600 font-semibold hover:text-stone-700"
+  >
+    CRA
+  </a>
+  <a
+    href="https://fresh.willcolton.com"
+    class="block px-2 py-1 text-stone-600 font-semibold hover:text-stone-700"
+  >
+    Deno Fresh
+  </a>
+  <a
+    href="https://svelte.willcolton.com"
+    class="block -mb-[2px] px-2 pt-1 pb-[2px] text-stone-600 font-bold hover:text-stone-700 border-b-4 border-stone-600 hover:border-stone-700"
+  >
+    Sveltekit
+  </a>
 </header>
