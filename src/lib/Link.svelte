97b6144217ecebcde56a7b4281885bcc4f23b27d
Will Colton <coltonw@gmail.com>
2026-04-14 21:31:18 -0400

    Formatting

--- src/lib/Link.svelte
+++ src/lib/Link.svelte
@@ -1,12 +1,7 @@
 <script lang="ts">
   export let href: string | undefined = undefined;
-  export let onClick: (() => void) | undefined = undefined;
 </script>
 
-<a
-  {href}
-  {onClick}
-  class="text-stone-600 font-bold hover:underline cursor-pointer"
->
+<a {href} class="text-stone-600 font-bold hover:underline cursor-pointer">
   <slot />
 </a>
